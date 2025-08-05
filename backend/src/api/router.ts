8cd654b5424d7303d3ea8263ca5e43c1abfe6592
artem <artemkka1035@gmail.com>
2025-08-06 01:15:32 +0300

    setup-backend

--- backend/src/api/router.ts
+++ backend/src/api/router.ts
@@ -5,11 +5,6 @@ import { versionService } from "./services/version";
 const router = (() => {
   const router = Router();
 
-  router.get("/test", (req, res) => {
-    console.log("test /api");
-    res.status(200).json({ message: "Hello World" });
-  });
-
   router.get(API_ENDPOINTS.VERSION, versionService.getServiceVersion);
 
   return router;
