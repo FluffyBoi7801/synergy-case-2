f2efb11db7afbfab03ebc251ca0065b709ea18e0
artem <artemkka1035@gmail.com>
2025-08-15 00:35:28 +0300

    add-toasts

--- web/src/shared/ui/Toaster/Toaster.tsx
+++ web/src/shared/ui/Toaster/Toaster.tsx
@@ -1,19 +1,11 @@
 import { Portal } from "@/shared/ui";
 import { useToaster } from "@/shared/ui/Toaster/hooks";
 import { AnimatePresence } from "framer-motion";
-import { Toast, ToastType } from "@/shared/ui/Toaster/components";
-import { useEffect } from "react";
+import { Toast } from "@/shared/ui/Toaster/components";
 import classes from "./Toaster.module.scss";
 
 export const Toaster = () => {
-  const { toasts, addToast } = useToaster();
-
-  useEffect(() => {
-    addToast({
-      type: ToastType.OK,
-      text: "Hello World",
-    });
-  }, []);
+  const { toasts } = useToaster();
 
   return (
     <Portal>
--- web/src/shared/ui/Toaster/components/Toast.tsx
+++ web/src/shared/ui/Toaster/components/Toast.tsx
@@ -1,7 +1,9 @@
-import { FC, PropsWithChildren, useEffect } from "react";
+import { FC, PropsWithChildren, useEffect, useState } from "react";
 import classes from "./Toast.module.scss";
 import { motion, Variants } from "framer-motion";
 import { useToaster } from "@/shared/ui/Toaster/hooks";
+import cn from "classnames";
+import { CheckIcon, CloseIcon, ErrorIcon } from "@/shared/icons";
 
 enum ToastState {
   INITIAL = "INITIAL",
@@ -20,33 +22,69 @@ const TOAST_STATES: Variants = {
   [ToastState.HIDDEN]: { opacity: 0 },
 };
 
+const TOAST_TIMEOUT = 6000;
+
+const TIMEOUT_DISCRETIZATION = 50;
+
 type Props = {
   id: string;
   type: ToastType;
 };
 
-// TODO: прогрессбар в виде полоски снизу
-
 export const Toast: FC<PropsWithChildren<Props>> = ({ id, type, children }) => {
+  let timer: NodeJS.Timeout;
   const { removeToast } = useToaster();
+  const [progress, setProgress] = useState(0);
+  const progressStep = (100 / (TOAST_TIMEOUT / TIMEOUT_DISCRETIZATION)).toFixed(
+    2
+  );
 
   useEffect(() => {
-    const timer = setTimeout(() => {
-      removeToast(id);
-    }, 6000);
+    timer = setInterval(() => {
+      console.log(progress);
+      if (progress + Number(progressStep) < 100) {
+        setProgress((prev) => Number((prev + Number(progressStep)).toFixed(2)));
+      } else if (progress === 100) {
+        removeToast(id);
+      } else {
+        setProgress(100);
+      }
+      console.log(progress);
+    }, TIMEOUT_DISCRETIZATION);
 
-    return () => clearTimeout(timer);
-  }, [id, removeToast]);
+    return () => clearInterval(timer);
+  }, [progress]);
+
+  const getToastIcon = (type: ToastType) => {
+    switch (type) {
+      case ToastType.OK:
+        return <CheckIcon />;
+      case ToastType.ERROR:
+        return <ErrorIcon />;
+    }
+  };
 
   return (
     <motion.div
-      className={classes.toast}
+      className={cn(classes.toast, classes[`toast_${type}`])}
       variants={TOAST_STATES}
       initial={ToastState.INITIAL}
       animate={ToastState.VISIBLE}
       exit={ToastState.HIDDEN}
+      data-progress={progress}
     >
+      {getToastIcon(type)}
       {children}
+      <button
+        className={classes.toast__closeIcon}
+        onClick={() => removeToast(id)}
+      >
+        <CloseIcon size={20} />
+      </button>
+      <span
+        style={{ width: `${progress}%` }}
+        className={classes.toast__progressbar}
+      />
     </motion.div>
   );
 };
